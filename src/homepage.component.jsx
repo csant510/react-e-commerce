beee2b30329931da428a0af977ad9d4cd8be631a
csant510 <james.sant83@gmail.com>
2020-03-24 00:17:35 -0700

    adding basic hompage setup to start will break every down to compents later

---
+++ src/homepage.component.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Homepage = () => (
+  <div classname="homepage">
+    <div className="directory-menu">
+      <div className="Menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="Menu-item">
+        <div className="content">
+          <h1 className="title">Jackets</h1>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="Menu-item">
+        <div className="content">
+          <h1 className="title">Sneakers</h1>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="Menu-item">
+        <div className="content">
+          <h1 className="title">Women</h1>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="Menu-item">
+        <div className="content">
+          <h1 className="title">Mens</h1>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Homepage;
