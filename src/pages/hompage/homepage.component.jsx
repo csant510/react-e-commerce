0486939851e214320349a8227e4ca85eef34a421
csant510 <james.sant83@gmail.com>
2020-03-24 02:29:56 -0700

    broken up hompage in to small compoments and styled menu itmes

---
+++ src/pages/hompage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../../componets/directory/directory.compoent";
+
+const Homepage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default Homepage;
